e4142f95a48f279c229f883238aa8a518989c8fb
vishwas1 <vishwasanand.cse@gmail.com>
2020-04-15 00:51:14 +0530

    crypto lib implemented

--- app/src/main/java/com/example/android/biometricauth/CryptoUtils.kt
+++ app/src/main/java/com/example/android/biometricauth/CryptoUtils.kt
@@ -1,18 +1,159 @@
 package com.example.android.biometricauth
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyPermanentlyInvalidatedException
 import android.security.keystore.KeyProperties
+import android.security.keystore.KeyProperties.BLOCK_MODE_CBC
+import android.security.keystore.KeyProperties.ENCRYPTION_PADDING_PKCS7
+import android.security.keystore.KeyProperties.KEY_ALGORITHM_AES
+
 import java.security.KeyStore
 import java.security.KeyStoreException
 import java.security.NoSuchAlgorithmException
 import java.security.NoSuchProviderException
-import javax.crypto.KeyGenerator
 
+import javax.crypto.KeyGenerator
+import javax.crypto.Cipher
+import javax.crypto.IllegalBlockSizeException
+import javax.crypto.KeyGenerator
+import javax.crypto.NoSuchPaddingException
+import javax.crypto.SecretKey
+
+
+class CryptoUtils(val android_key: String) {
+
+    private lateinit var keyStore: KeyStore
+    private lateinit var keyGenerator: KeyGenerator
+    
+    fun setupKeyStoreAndKeyGenerator() {
+        try {
+            this.keyStore = KeyStore.getInstance(android_key)
+        } catch (e: KeyStoreException) {
+            throw RuntimeException("Failed to get an instance of KeyStore", e)
+        }
+
+        try {
+            keyGenerator = KeyGenerator.getInstance(KEY_ALGORITHM_AES, android_key)
+        } catch (e: Exception) {
+            when (e) {
+                is NoSuchAlgorithmException,
+                is NoSuchProviderException ->
+                    throw RuntimeException("Failed to get an instance of KeyGenerator", e)
+                else -> throw e
+            }
+        }
+    }
+
+    fun setupCiphers(): Pair<Cipher, Cipher> {
+        val defaultCipher: Cipher
+        val cipherNotInvalidated: Cipher
+        try {
+            val cipherString = "$KEY_ALGORITHM_AES/$BLOCK_MODE_CBC/$ENCRYPTION_PADDING_PKCS7"
+            defaultCipher = Cipher.getInstance(cipherString)
+            cipherNotInvalidated = Cipher.getInstance(cipherString)
+        } catch (e: Exception) {
+            when (e) {
+                is NoSuchAlgorithmException,
+                is NoSuchPaddingException ->
+                    throw RuntimeException("Failed to get an instance of Cipher", e)
+                else -> throw e
+            }
+        }
+        return Pair(defaultCipher, cipherNotInvalidated)
+    }
+
+    fun createKey(keyName: String, invalidatedByBiometricEnrollment: Boolean) {
+        // The enrolling flow for fingerprint. This is where you ask the user to set up fingerprint
+        // for your flow. Use of keys is necessary if you need to know if the set of enrolled
+        // fingerprints has changed.
+        try {
+            this.keyStore.load(null)
+
+            val keyProperties = KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+            val builder = KeyGenParameterSpec.Builder(keyName, keyProperties)
+                    .setBlockModes(BLOCK_MODE_CBC)
+                    .setUserAuthenticationRequired(true)
+                    .setEncryptionPaddings(ENCRYPTION_PADDING_PKCS7)
+                    .setInvalidatedByBiometricEnrollment(invalidatedByBiometricEnrollment)
+
+            keyGenerator.run {
+                init(builder.build())
+                generateKey()
+            }
+        } catch (e: Exception) {
+            when (e) {
+                is NoSuchAlgorithmException,
+                is InvalidAlgorithmParameterException,
+                is CertificateException,
+                is IOException -> throw RuntimeException(e)
+                else -> throw e
+            }
+        }
+    }
+    
+    fun initCipher(cipher: Cipher, keyName: String): Boolean {
+        try {
+            keyStore.load(null)
+            cipher.init(Cipher.ENCRYPT_MODE, keyStore.getKey(keyName, null) as SecretKey)
+            return true
+        } catch (e: Exception) {
+            when (e) {
+                is KeyPermanentlyInvalidatedException -> return false
+                is KeyStoreException,
+                is CertificateException,
+                is UnrecoverableKeyException,
+                is IOException,
+                is NoSuchAlgorithmException,
+                is InvalidKeyException -> throw RuntimeException("Failed to init Cipher", e)
+                else -> throw e
+            }
+        }
+    }
+
+    fun tryEncrypt(message: String, cipher: Cipher): ByteArray {
+        try {
+            return cipher.doFinal(message.toByteArray())
+        } catch (e: Exception) {
+            when (e) {
+                is BadPaddingException,
+                is IllegalBlockSizeException -> {
+                    Toast.makeText(this, "Failed to encrypt the data with the generated key. "
+                            + "Retry the purchase", Toast.LENGTH_LONG).show()
+                    Log.e(TAG, "Failed to encrypt the data with the generated key. ${e.message}")
+                }
+                else -> throw e
+            }
+        }
+    }
+
+    // fun decryptData(ivBytes: ByteArray, data: ByteArray): String{
+    //     val cipher = Cipher.getInstance("AES/CBC/NoPadding")
+    //     val spec = IvParameterSpec(ivBytes)
+
+    //     cipher.init(Cipher.DECRYPT_MODE, getKey(), spec)
+    //     return cipher.doFinal(data).toString(Charsets.UTF_8).trim()
+    // }
+}
+
+
+interface ICryptoUtils {
+    fun setup()
+    fun encrypt(raw_message: String): ByteArray 
+    fun decrypt(encrypted_message: String): String
+}
+    
+
+
+/*
 class CryptoUtils() {
     private lateinit var keyStore: KeyStore
     private lateinit var keyGenerator: KeyGenerator
-    /**
-     * Sets up KeyStore and KeyGenerator
-     */
+  
     private fun setupKeyStoreAndKeyGenerator() {
         try {
             keyStore = KeyStore.getInstance(MainActivity.ANDROID_KEY_STORE)
@@ -37,3 +178,6 @@ class CryptoUtils() {
         private const val TAG = "MainActivity"
     }
 }
+
+*/
+
--- app/src/main/java/com/example/android/biometricauth/WelcomeActivity.kt
+++ app/src/main/java/com/example/android/biometricauth/WelcomeActivity.kt
@@ -43,8 +43,8 @@ import javax.crypto.NoSuchPaddingException
 import javax.crypto.SecretKey
 
 
-class WelcomeActivity : AppCompatActivity() {
-
+class WelcomeActivity : AppCompatActivity(), CryptoUtils {
+     private val cryptoUtilsObj: CryptoUtils = CryptoUtils(ANDROID_KEY_STORE)
      override fun onCreate(savedInstanceState: Bundle?) {
 
          super.onCreate(savedInstanceState)
@@ -61,46 +61,78 @@ class WelcomeActivity : AppCompatActivity() {
              text = Decrypted
          }
 
-         initializeViews()
+         cryptoUtilsObj.setupKeyStoreAndKeyGenerator()
+         val (defaultCipher: Cipher, cipherNotInvalidated: Cipher) = cryptoUtilsObj.setupCiphers()
+         
+         initializeViews(defaultCipher, cipherNotInvalidated)
      }
-    private fun initializeViews() {
+    private fun initializeViews(cipherNotInvalidated: Cipher, defaultCipher: Cipher) {
         val button = findViewById<Button>(R.id.decrypt_button)
+        cryptoUtilsObj.createKey(DEFAULT_KEY_NAME)
         button.setOnClickListener {
-            showBiometricPrompt1()
+            showBiometricPrompt(defaultCipher,DEFAULT_KEY_NAME)
         }
     }
 
-    private fun showBiometricPrompt1() {
+
+    private fun showBiometricPrompt(
+        cipher: Cipher,
+        keyName: String
+    ) {
         val myPreference = HsPreference(this)
 
-        val biometricPromptUtils = BiometricPromptUtils(this, object : BiometricPromptUtils.BiometricListener {
-            override fun onAuthenticationLockoutError() {
-                // implement your lockout error UI prompt
-            }
+        if (cryptoUtilsObj.initCipher(cipher, keyName)){
+            val biometricPromptUtils = BiometricPromptUtils(this, object : BiometricPromptUtils.BiometricListener {
+                override fun onAuthenticationLockoutError() {
+                    // implement your lockout error UI prompt
+                }
+    
+                override fun onAuthenticationPermanentLockoutError() {
+                    // implement your permanent lockout error UI prompt
+                }
+    
+                override fun onAuthenticationSuccess() {
+                    // implement your authentication success UI prompt
+                    // val Message = myPreference.getEncrypted()
+                    val message_to_entrypt = "Hello World"
+                    val encrypted_message = cryptoUtilsObj.tryEncrypt(message_to_entrypt, cipher);
 
-            override fun onAuthenticationPermanentLockoutError() {
-                // implement your permanent lockout error UI prompt
-            }
+                    val encryptedText = Base64.encodeToString(encrypted_message, 0 /* flags */)
 
-            override fun onAuthenticationSuccess() {
-                // implement your authentication success UI prompt
-                val Message = myPreference.getEncrypted()
+                    findViewById<TextView>(R.id.encrypted_message).run {
+                        text = encryptedText
+                    }
+                    findViewById<TextView>(R.id.decrypted_message).run {
+                        text = message_to_entrypt
+                    }
 
-            }
+    
+                }
+    
+                override fun onAuthenticationFailed() {
+                    // implement your authentication failed UI prompt
+                }
+    
+                override fun onAuthenticationError() {
+                    // implement your authentication error UI prompt
+                }
+    
+            })
+            biometricPromptUtils.showBiometricPrompt(
+                    resources.getString(R.string.confirmDescription),
+                    resources.getString(R.string.cancelKey),
+                    confirmationRequired = true
+            )
+        }
 
-            override fun onAuthenticationFailed() {
-                // implement your authentication failed UI prompt
-            }
+        
+    }
 
-            override fun onAuthenticationError() {
-                // implement your authentication error UI prompt
-            }
-
-        })
-        biometricPromptUtils.showBiometricPrompt(
-                resources.getString(R.string.confirmDescription),
-                resources.getString(R.string.cancelKey),
-                confirmationRequired = true
-        )
+    companion object {
+        private const val ANDROID_KEY_STORE = "AndroidKeyStore"
+        private const val DIALOG_FRAGMENT_TAG = "myFragment"
+        private const val KEY_NAME_NOT_INVALIDATED = "key_not_invalidated"
+        private const val SECRET_MESSAGE = "Very secret message"
+        private const val TAG = "MainActivity"
     }
 }
